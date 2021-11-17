c5a17ac3ca4c6d96473da35f529f67c657dc723f
irik77587 <irik77587@gmail.com>
2021-11-17 21:59:43 +0600

    builtwith webpack@4

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App(){
+	return (
+		<h1>React no CRA</h1>
+	);
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  // [webpack-dev-server] "hot: true" automatically applies HMR plugin, you don't have to add it manually to your webpack configuration.
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+  	// contentBase is deprecated by static in webpack v5
+    contentBase: path.resolve(__dirname, "./dist"),
+    hot: true,
+  },
+};
